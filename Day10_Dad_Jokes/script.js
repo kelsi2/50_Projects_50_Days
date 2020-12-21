543f6b54cf784fd68586407f3f520c6aab779d70
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-12-20 16:54:59 -0800

    Day 10 project complete

---
+++ Day10_Dad_Jokes/script.js
@@ -0,0 +1,37 @@
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+// Using async await
+async function generateJoke() {
+  const config = {
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+
+  const res = await fetch('https://icanhazdadjoke.com', config);
+
+  const data = await res.json();
+
+  jokeElement.innerHTML = data.joke;
+}
+
+//Using .then() [promises]
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       'Accept': 'application/json'
+//     }
+//   };
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then(res => res.json())
+//     .then((data) => {
+//       jokeElement.innerHTML = data.joke
+//     });
+// }
